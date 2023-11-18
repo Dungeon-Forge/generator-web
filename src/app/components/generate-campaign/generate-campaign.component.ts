7883767786749a029cddfa86033955dc3dc85439
Adam Ure <adamqure@icloud.com>
2023-11-18 09:58:52 -0700

    Create campaign form (#31)

--- src/app/components/generate-campaign/generate-campaign.component.ts
+++ src/app/components/generate-campaign/generate-campaign.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generate-campaign',
   templateUrl: './generate-campaign.component.html',
-  styleUrls: ['./generate-campaign.component.less']
+  styleUrls: ['./generate-campaign.component.less'],
 })
 export class GenerateCampaignComponent {
+  numCharacters: number = 0
 
+  decrementNumCharacters() {
+    this.numCharacters = Math.max(0, this.numCharacters - 1)
+  }
+
+  incrementNumCharacters() {
+    this.numCharacters = Math.min(10, this.numCharacters + 1)
+  }
+
+  submitSurvey() {
+    const surveyOutput = {
+      "NumCharacters": this.numCharacters,
+      "StartingLevel": (<HTMLInputElement>document.getElementById("start-level"))?.value,
+      "EndingLevel": (<HTMLInputElement>document.getElementById("end-level"))?.value,
+      "RolePlaying": (<HTMLInputElement>document.querySelector('input[name="Role Playing"]:checked')).value,
+      "Traps": (<HTMLInputElement>document.querySelector('input[name="Traps"]:checked')).value,
+      "Puzzles": (<HTMLInputElement>document.querySelector('input[name="Puzzles"]:checked')).value,
+      "Combat": (<HTMLInputElement>document.querySelector('input[name="Combat"]:checked')).value
+    }
+    console.log("New Combat Value: " + JSON.stringify(surveyOutput));
+  }
 }
