67a36af727ed8aabc3829b65c841b12de8872d63
Adam Ure <adamqure@icloud.com>
2023-11-21 23:56:26 -0700

    CORS support

--- src/app/services/remote-campaign-service.ts
+++ src/app/services/remote-campaign-service.ts
@@ -1,11 +1,34 @@
 import { Campaign } from "../models/campaign";
 import { CampaignFormResponse } from "../models/campaign-form-response";
 import { CampaignService } from "./campaign-service";
+import axios from "axios";
 
 export class RemoteCampaignService implements CampaignService {
-    generateCampaign(input: CampaignFormResponse): Promise<string> {
-        throw new Error("Method not implemented.");
+    // baseURL = "http://dungeonforgeserver-env.eba-6x2ib96k.us-east-2.elasticbeanstalk.com"
+    baseURL = "http://localhost:3000"
+
+    constructor() {
+        axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*' 
     }
+
+    generateCampaign(input: CampaignFormResponse): Promise<string> {
+        const url = "/campaigns/generate"
+
+        return new Promise((resolve, reject) => {
+            axios.post(this.baseURL + url, input)
+            .then(function (response) {
+                console.log("Response headers: " + response.headers)
+                console.log("Received response from campaign generator: " + response)
+                resolve(response.data.campaignId)
+            })
+            .catch(function (error) {
+                console.log("Error headers: " + error.headers);
+                console.log("Error generating campaign: " + error);
+                reject(error);
+            })
+        })
+    }
+
     fetchCampaign(id: string): Promise<Campaign> {
         throw new Error("Method not implemented.");
     }
